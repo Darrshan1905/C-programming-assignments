6799e3cfd78c379a67c4a010f3ee2e3b8231962c
Darrshan1905 <darrshan@mobicip.com>
2023-12-20 13:38:05 +0530

    added ex.3 and 4

---
+++ Chapter4/exp4.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+
+#define MAXVAL 100
+
+int sp = 0;
+double stack[MAXVAL];
+
+void push(double f) {
+	if(sp == -1)
+		sp = 0;
+	if(sp <  MAXVAL)
+		stack[sp++] = f;
+        else
+		printf("Stack overflow\n");	
+}
+
+double pop() {
+	double c;
+	if(sp > 0)
+		return c = stack[--sp];
+	else {
+		printf("Stack underflow\n");
+		return -1;
+
+	}
+}
+
+double printTop() {
+	if(sp > 0)
+		return stack[sp - 1];
+	else 
+		printf("Stack underflow");
+}
+
+void swapTopTwo() {
+	if(sp > 1) {
+		stack[sp - 1] = stack[sp - 1] + stack[sp - 2];
+		stack[sp - 2] = stack[sp - 1] - stack[sp - 2];
+		stack[sp - 1] = stack[sp - 1] - stack[sp - 2];	
+
+		printf("\nNow the top 2 elemnets are : %lf %lf\n", stack[sp - 1], stack[sp - 2]);
+	}
+	else {
+		printf("Stack underflow");
+	}
+}
+
+void clearStack() {
+	sp = -1;
+	printf("\nStack cleared\n");
+}
+
+int main() {
+	push(4);
+	push(3);
+	push(2);
+	push(1);
+
+	printf("Current stack is 1 2 3 4\n");
+
+	printf("Printing the top element of the stack:");
+	printf("%lf\n", printTop());
+
+	printf("Swapping the top 2 elements of the stack:");
+	swapTopTwo();
+
+	printf("Clearing the stack:");
+	clearStack();
+	pop();
+}
